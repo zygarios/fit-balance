e43eda1d3b8c18d6285c5e96f91bb71447eaccf2
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2020-07-06 19:03:49 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCbQIwdw81XRteEUvI0r3Ex3u-kzbPsHc4",
+    authDomain: "fit-balance-b4673.firebaseapp.com",
+    databaseURL: "https://fit-balance-b4673.firebaseio.com",
+    projectId: "fit-balance-b4673",
+    storageBucket: "fit-balance-b4673.appspot.com",
+    messagingSenderId: "1080523647410",
+    appId: "1:1080523647410:web:3c82432bbcae27c2b79cab",
+    measurementId: "G-KVHY7GQ7HW"
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
